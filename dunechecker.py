0fb988922f25266e64b0ac1212b625893b461ee2
kaxaru <khidechi@gmail.com>
2023-05-03 15:47:26 +0300

    new version

--- dunechecker.py
+++ dunechecker.py
@@ -1,4 +1,4 @@
-from utils import get_main_wallet, get_all_wallets
+# from utils import get_main_wallet, get_all_wallets
 from requests import Session
 from pyuseragents import random as random_useragent
 import json
@@ -6,34 +6,41 @@ from loguru import logger
 from web3 import Web3
 from multiprocessing.dummy import Pool
 from enum import Enum
+import csv
+import math
 
 import os
 
 class queryID(Enum):
-    checkUSD = 2350073
-    checkETH = 2350748
-    checkBNBChain = 2344893
+    checkUSD        = 2350073
+    checkETH        = 2350748
+    checkBNBChain   = 2344893
 
 
-multith = str(input("multithreading? - y/n \n"))
-if multith == 'Y' or multith == 'y':
-    threads = int(input("number of threads? \n"))
-else:
-    threads = 1
-pool = Pool(threads)
+threads = 1
+pool    = Pool(threads)
 
 query_res = {}
 for q in queryID:
     query_res[q.name] = []
 
+def round_to(num, digits=2):
+    try:
+        if num == 0: return 0
+        scale = int(-math.floor(math.log10(abs(num - int(num))))) + digits - 1
+        if scale < digits: scale = digits
+        return round(num, scale)
+    except: return num
+
 def search_wallet_in_table(d):
     _data = d["q"]
     name_column = d["column"]
     wallet = d["wal"]
+
     name_query = d["name"]
     for row in _data:
-        if row[name_column] == wallet["wallet"].address.lower():
-            query_res[name_query].append({"address": wallet['wallet'].address, "user_tx_count": row['user_tx_count'], "user_tx_value": row['usd_tx_value']})
+        if row[name_column] == wallet.lower():
+            query_res[name_query].append({"address": wallet, "user_tx_count": row['user_tx_count'], "user_tx_value": row['usd_tx_value']})
 
 def check_wallets(wallets):
     for enum_q in queryID:
@@ -82,6 +89,7 @@ def check_wallets(wallets):
         q_resp_data = json.loads(data.content)["data"]["get_execution"]["execution_succeeded"]["data"]
         q_resp_arr.append({q.name: q_resp_data})
 
+
     logger.info('arr q_checkUSD_map')
     q_checkUSD_map = [{"q": q_resp_arr[list(queryID).index(queryID.checkUSD)][queryID.checkUSD.name], "column": "sender", "wal": wal, "name": queryID.checkUSD.name} for wal in wallets]
     logger.info('arr q_checkEth_map')
@@ -99,16 +107,81 @@ def check_wallets(wallets):
     pool.map(search_wallet_in_table, q_checkBNBChain_map)
     q_checkBNBChain_map = []
 
-    for enum_q in queryID:
-        filename = enum_q.name
-        _q_resp = query_res[filename]
-        for row in _q_resp:
-            with open(f'{enum_q.name}.txt', 'a+') as file:
-                file.write(f'{json.dumps(row)}\n')
+def create_file(file_name, query_res):
 
+    with open(f'{outfile}{file_name}.csv', 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+
+        spamwriter.writerow([
+            'number', 
+            'wallet', 
+            'checkUSD-count', 
+            'checkUSD-value', 
+            'checkETH-count', 
+            'checkETH-value', 
+            'checkBNBChain-count', 
+            'checkBNBChain-value'
+            ])
+
+        wallets = {}
+
+        for data in query_res.items():
+            for items in data[1]:
+                address = items['address']
+                wallets.update({address : {}})
+
+        for data in query_res.items():
+
+            type_check = data[0]
+
+            for items in data[1]:
+                address = items['address']
+                user_tx_count = items['user_tx_count']
+                user_tx_value = items['user_tx_value']
+                wallets[address].update({type_check : {'user_tx_count' : user_tx_count, 'user_tx_value' : user_tx_value}})
+
+        # create csv
+        zero = 0
+        for wallet in wallets.items():
+            zero += 1
+
+            massive = []
+
+            massive.append(zero)
+            massive.append(wallet[0])
+
+            try:
+                massive.append(wallet[1]['checkUSD']['user_tx_count'])
+                massive.append(round_to(wallet[1]['checkUSD']['user_tx_value']))
+            except: 
+                massive.append(0)
+                massive.append(0)
+
+            try:
+                massive.append(wallet[1]['checkETH']['user_tx_count'])
+                massive.append(round_to(wallet[1]['checkETH']['user_tx_value']))
+            except: 
+                massive.append(0)
+                massive.append(0)
+
+            try:
+                massive.append(wallet[1]['checkBNBChain']['user_tx_count'])
+                massive.append(round_to(wallet[1]['checkBNBChain']['user_tx_value']))
+            except: 
+                massive.append(0)
+                massive.append(0)
+
+            spamwriter.writerow(massive)
 
 if __name__ == '__main__':
-    wallets = get_all_wallets(get_main_wallet())
+
+    outfile = ''
+    with open(f"{outfile}wallets.txt", "r") as f:
+        wallets = [row.strip() for row in f]
+
     check_wallets(wallets)
+
+    file_name = 'results'
+    create_file(file_name, query_res)
 
 
---
+++ readme.md
@@ -0,0 +1,2 @@
+Here is the version of the script that https://t.me/zaivanza made
+The script works with public address and outputs data in csv format from dune.
